c7d7025cd9721a4edaa6e826a28186d632603b05
braddefauw <badefauw@gmail.com>
2022-03-15 08:39:20 -0500

    initial operations

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("test")
+const sum = function(arr) {
+    return arr.reduce((a, b) => {
+        return a + b;
+    }, 0);
+};
+
+const diff = function(arr) {
+    return arr.reduce((a, b) => {
+        return -a - b;
+    }, 0);
+};
+
+const mult = function(arr) {
+    return arr.reduce((a, b) => {
+        return a * b;
+    });
+};
+const divide = function(arr) {
+    return arr.reduce((a, b) => {
+        return a / b;
+    });
+};
+
+const power = function(a, b) {
+	return a ** b;
+};
+
+const factorial = function(num) {
+	result = num;
+  if(num === 0){
+    return 1;
+  }
+  while(num > 1){
+    num--;
+    result *= num;
+  }
+  return result;
+};
