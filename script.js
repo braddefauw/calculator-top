d355d1faae31c700494fb9705f32df34b0400495
braddefauw <badefauw@gmail.com>
2022-03-21 11:23:51 -0500

    keypress testing

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ for(const btn of numBtns){
     btn.addEventListener('click', function (){
         if(this.value === "."){
             decimalCount++;
-            console.log(decimalCount);
         }
         if(this.value === "." && decimalCount > 1){
             return;
@@ -46,6 +45,7 @@ resetBtn.addEventListener('click', function () {
     displayValue.innerText = "";
     valueOne = "";
     valueTwo = "";
+    result = "";
 })
 
 let equalBtn = document.querySelector(".equals")
@@ -84,16 +84,38 @@ document.addEventListener('keypress', function(e){
     console.log(e.key);
     if (e.key >= 0 && e.key <= 9){
         displayValue.innerText += `${e.key}`;
+    }else if(e.key === "."){
+        displayValue.innerText += `${e.key}`;
+        decimalCount++;
     }else if(e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/"){
         if(valueOne !== "" && displayValue.innerText.indexOf(valueOne) !== -1){
             operate();
-            displayValue.innerText += e.key; 
+            displayValue.innerText += `${e.key}`; 
         }else{
             valueOne = parseFloat(displayValue.innerText);
-            displayValue.innerText += e.key; 
+            displayValue.innerText += `${e.key}`; 
         }
         decimalCount = 0;
     }else if(e.key === "Enter"){
-        operate();
+        if(displayValue.innerText.indexOf("+") !== -1){
+            valueTwo = parseFloat(displayValue.innerText.substring(displayValue.innerText.indexOf('+')+1));
+            result = Math.round((valueOne + valueTwo)*100)/100;
+        }else if(displayValue.innerText.indexOf("-") !== -1){
+            valueTwo = parseFloat(displayValue.innerText.substring(displayValue.innerText.indexOf('-')+1));
+            result = Math.round((valueOne - valueTwo)*100)/100;
+        }else if(displayValue.innerText.indexOf("*") !== -1){
+            valueTwo = parseFloat(displayValue.innerText.substring(displayValue.innerText.indexOf('*')+1));
+            result = Math.round((valueOne * valueTwo)*100)/100;
+        }else if(displayValue.innerText.indexOf("/") !== -1){
+            valueTwo = parseFloat(displayValue.innerText.substring(displayValue.innerText.indexOf('/')+1));
+            if(valueTwo === 0){
+                result = "You can't divide by zero, fool!"
+            }else{
+                result = Math.round((valueOne / valueTwo)*100)/100;
+            }
+        }
+        displayValue.innerText = result;
+        valueOne = result;
     }
+    console.log(result, displayValue.innerText);
 })
